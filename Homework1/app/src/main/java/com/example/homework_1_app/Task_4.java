137e43f869b693df089f69c32693b33035016a6b
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-04-20 11:17:28 -0700

    Allows user to swipe to view movie images

--- Homework1/app/src/main/java/com/example/homework_1_app/Task_4.java
+++ Homework1/app/src/main/java/com/example/homework_1_app/Task_4.java
@@ -1,6 +1,7 @@
 package com.example.homework_1_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.content.ContextCompat;
 import androidx.core.view.GestureDetectorCompat;
@@ -24,19 +25,23 @@ public class Task_4 extends AppCompatActivity {
 
     private static final String TAG = Task_4.class.getSimpleName();
 
-    private GestureDetectorCompat gestureDetector;
+    private GestureDetectorCompat mDetector;
     ImageView imageView;
     SeekBar seekBar;
     MovieData movieData = new MovieData();
     CoordinatorLayout coordinatorLayout;
+    int index =  0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_4);
 
+        mDetector = new GestureDetectorCompat(getApplicationContext(), new MyGestureListener());
+
+
         imageView = findViewById(R.id.movie_image_view);
-        imageView.setImageResource(R.drawable.alice);
+        imageView.setImageResource((Integer) movieData.getItem(0).get("image"));
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,7 +61,7 @@ public class Task_4 extends AppCompatActivity {
                 return true;
             }
         });
-        
+
 
         seekBar = findViewById(R.id.seek_bar);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -80,6 +85,81 @@ public class Task_4 extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final String DEBUG_TAG = "Gestures";
+
+        private static final int MIN_SWIPE_DISTANCE_X_AXIS = 100;
+        private static final int MIN_SWIPE_DISTANCE_Y_AXIS = 100;
+
+        //Basing this upon device... currently running a Google Pixel 3XL
+        //The maximum distance I can swipe from edge to edge is approx 1400 x axis
+        private static final int MAX_SWIPE_DISTANCE_X_AXIS = 1100;
+        private static final int MAX_SWIPE_DISTANCE_Y_AXIS = 1100;
+
+
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            Log.d(DEBUG_TAG, "onFling event 1: " + e1.toString());
+            Log.d(DEBUG_TAG, "onFling event 2: " + e2.toString());
+            Log.d(DEBUG_TAG, "onFling velocity x: " + velocityX);
+            Log.d(DEBUG_TAG, "onFling velocity y: " + velocityY);
+
+            double changeInXAxis = e1.getX() - e2.getX();
+            double changeInYAxis = e1.getY() - e2.getY();
+
+            Log.d(DEBUG_TAG, "changeInXAxis " + changeInXAxis);
+            Log.d(DEBUG_TAG, "changeInYAxis " + changeInYAxis);
+
+            if((Math.abs(changeInXAxis)>= MIN_SWIPE_DISTANCE_X_AXIS) && (Math.abs(changeInXAxis) <= MAX_SWIPE_DISTANCE_X_AXIS)) {
+                Log.d(DEBUG_TAG, "here");
+                if (changeInXAxis > 0) {
+
+                    try {
+                        Log.d(DEBUG_TAG, "Decrement Index!!!!!!!!!!!!");
+
+                        if (index == 0) {
+
+                        } else {
+                            index --;
+                            imageView.setImageResource((Integer) movieData.getItem(index).get("image"));
+                        }
+
+
+
+                    }catch (IndexOutOfBoundsException | NullPointerException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                if (changeInXAxis < 0) {
+                    Log.d(DEBUG_TAG, "Increment Index!!!!!!!!!!!!");
+                    index++;
+                    imageView.setImageResource((Integer) movieData.getItem(index).get("image"));
+                }
+            }
+
+//            index++;
+//            imageView.setImageResource((Integer) movieData.getItem(index).get("image"));
+
+
+            return true;
+        }
+
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            Log.d(DEBUG_TAG, "onLongPress: " + e.toString());
+            seekBar.setProgress(50);
+        }
     }
 
     public void scaleImage(ImageView image, double progressPercentage) {
