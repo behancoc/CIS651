b7e71c8c7b80e32cf12d331561ac2e3a8a8b7753
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-05-04 17:25:19 -0700

    Implements animation on image view for 4 of 6 buttons

--- Lab5/app/src/main/java/com/example/lab5application/MainActivity.java
+++ Lab5/app/src/main/java/com/example/lab5application/MainActivity.java
@@ -3,12 +3,69 @@ package com.example.lab5application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView imageView = (ImageView) findViewById(R.id.image_view);
+        imageView.setImageResource(R.drawable.alice);
+
+        final Button nextActivityButton = (Button) findViewById(R.id.next_activity_button);
+        nextActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        final Button moveAndRotateButton = (Button) findViewById(R.id.move_and_rotate_button);
+        moveAndRotateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.animate().setDuration(1000).x(500).y(800).rotationYBy(720).scaleX(.4F).scaleY(.4f);
+            }
+        });
+
+        final Button moveBackButton = (Button) findViewById(R.id.move_back_button);
+        moveBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.animate().setDuration(1000).x(imageView.getLeft()).y(imageView.getTop()).rotationYBy(720).scaleX(1F).scaleY(1F);
+            }
+        });
+
+        final Button fadeOutButton = (Button) findViewById(R.id.fade_out_button);
+        fadeOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.animate().setDuration(1000).alpha(0F);
+            }
+        });
+
+        final Button fadeInButton = (Button) findViewById(R.id.fade_in_button);
+        fadeInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.animate().setDuration(1000).alpha(1F);
+            }
+        });
+
+
+        final Button xmlAnimatorButton = (Button) findViewById(R.id.xml_animator_button);
+        xmlAnimatorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
