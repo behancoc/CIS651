f203bf82a39f0c32d8d5dc6f0423f4b8f87450af
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-05-18 18:03:18 -0700

    Updates to home activity

--- Lab7/app/src/main/java/com/bhancock/lab7application/HomeActivity.java
+++ Lab7/app/src/main/java/com/bhancock/lab7application/HomeActivity.java
@@ -3,12 +3,43 @@ package com.bhancock.lab7application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        new CountDownTimer(3000, 1000) {
+            public void onTick(long millisecondsUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
     }
 }
