19b043732a011c0acb5731eee8dbdab71c9479bd
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-05-24 21:45:37 -0700

    Transitions Main Activity to Login Activity or Home Activity

--- Lab7/app/src/main/java/com/bhancock/lab7application/MainActivity.java
+++ Lab7/app/src/main/java/com/bhancock/lab7application/MainActivity.java
@@ -2,13 +2,51 @@ package com.bhancock.lab7application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new CountDownTimer(3000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+                if(mFirebaseUser == null) {
+                    startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                    finish();
+                }
+                 else {
+                    Toast.makeText(getApplicationContext(),
+                            "Please verify your email and login.", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                    finish();
+                }
+            }
+        }.start();
     }
 }
