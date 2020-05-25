a41115432afbb5e9897de60eca5d727eb53ae242
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-05-24 23:12:32 -0700

    Implements onclick listeners for buttons and saveToDB method

--- Lab7/app/src/main/java/com/bhancock/lab7application/LoginActivity.java
+++ Lab7/app/src/main/java/com/bhancock/lab7application/LoginActivity.java
@@ -1,14 +1,142 @@
 package com.bhancock.lab7application;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+    private EditText mDisplayNameEditText;
+    private EditText mPhoneNumberEditText;
+
+    Button mSignUpButton;
+    Button mResetPasswordButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        mEmailEditText = findViewById(R.id.email);
+        mPasswordEditText = findViewById(R.id.password);
+        mDisplayNameEditText = findViewById(R.id.display_name);
+        mPhoneNumberEditText = findViewById(R.id.phone_number);
+
+        mSignUpButton = findViewById(R.id.signup_button);
+        mResetPasswordButton = findViewById(R.id.reset_password_button);
+
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mEmailEditText.getText().toString().equals("")
+                        || mPasswordEditText.getText().toString().equals("")
+                        || mPhoneNumberEditText.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Please provide all necessary information",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mFirebaseAuth.createUserWithEmailAndPassword(
+                        mEmailEditText.getText().toString(),
+                        mPasswordEditText.getText().toString()).addOnSuccessListener(
+                                LoginActivity.this, new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        mFirebaseUser = authResult.getUser();
+                        mFirebaseUser.sendEmailVerification().addOnSuccessListener(
+                                LoginActivity.this, new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(getApplicationContext(),
+                                        "SignUp successful.  Verification email sent!",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(LoginActivity.this, new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getApplicationContext(),
+                                        e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }).addOnFailureListener(LoginActivity.this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(),
+                                e.getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+
+        mResetPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mEmailEditText.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(),
+                            "Please enter your email address", Toast.LENGTH_SHORT).show();
+                }
+                mFirebaseAuth.sendPasswordResetEmail(mEmailEditText.getText().toString())
+                        .addOnFailureListener(LoginActivity.this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this,
+                                e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnSuccessListener(LoginActivity.this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(LoginActivity.this,
+                                "Email sent!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        updateUI();
+    }
+
+
+    private void updateUI() {
+        if (mFirebaseUser != null) {
+            findViewById(R.id.display_name).setVisibility(View.GONE);
+            findViewById(R.id.phone_number).setVisibility(View.GONE);
+            mSignUpButton.setVisibility(View.GONE);
+        }
+    }
+
+    private void saveUserDataToDB() {
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference userReference = firebaseDatabase.getReference("Users");
+        userReference.child(mFirebaseUser.getUid())
+                .setValue(new User(mDisplayNameEditText.getText().toString(),
+                                   mEmailEditText.getText().toString(),
+                                   mPhoneNumberEditText.getText().toString()));
     }
 }
