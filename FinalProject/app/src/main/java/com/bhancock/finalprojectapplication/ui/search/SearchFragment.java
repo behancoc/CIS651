1330d1ac23f5347438cb3d03d3f7b7542a0a975e
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-06-02 19:32:04 -0700

    Uniformity in fragments

--- FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/ui/search/SearchFragment.java
+++ FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/ui/search/SearchFragment.java
@@ -1,5 +1,6 @@
 package com.bhancock.finalprojectapplication.ui.search;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -11,12 +12,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.bhancock.finalprojectapplication.R;
+import com.bhancock.finalprojectapplication.ui.profile.ProfileViewModel;
 
 public class SearchFragment extends Fragment {
 
-    private SearchViewModel mViewModel;
+    private SearchViewModel searchViewModel;
 
     public static SearchFragment newInstance() {
         return new SearchFragment();
@@ -25,13 +28,25 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+
+        searchViewModel =
+                ViewModelProviders.of(this).get(SearchViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        final TextView textView = root.findViewById(R.id.text_profile);
+        searchViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(SearchViewModel.class);
+        searchViewModel = ViewModelProviders.of(this).get(SearchViewModel.class);
         // TODO: Use the ViewModel
     }
 
