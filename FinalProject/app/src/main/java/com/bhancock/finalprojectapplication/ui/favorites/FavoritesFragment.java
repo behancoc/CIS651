ad98895d06739c769d66ed91b0516f899df5890e
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-06-06 21:09:32 -0700

    Starter list view for favorites tab within fragment

--- FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/ui/favorites/FavoritesFragment.java
+++ FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/ui/favorites/FavoritesFragment.java
@@ -1,28 +1,63 @@
 package com.bhancock.finalprojectapplication.ui.favorites;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bhancock.finalprojectapplication.R;
+import com.bhancock.finalprojectapplication.adapter.VisitedPlacesAdapter;
+import com.bhancock.finalprojectapplication.model.VisitedPlaces;
+
+import java.util.List;
 
 public class FavoritesFragment extends Fragment {
 
     private FavoritesViewModel favoritesViewModel;
+    private RecyclerView mFavoritesRecyclerView;
+    private VisitedPlacesAdapter mAdapter;
+    private ProgressBar mProgressBar;
+    private Context mContext;
+
+    private void initRecyclerView() {
+        mContext = getActivity().getApplicationContext();
+        mAdapter = new VisitedPlacesAdapter(getContext(), favoritesViewModel.getUserPlaces().getValue());
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(mContext);
+        mFavoritesRecyclerView.setLayoutManager(layoutManager);
+        mFavoritesRecyclerView.setAdapter(mAdapter);
+
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        favoritesViewModel =
-                ViewModelProviders.of(this).get(FavoritesViewModel.class);
+
+        favoritesViewModel = new ViewModelProvider(this).get(FavoritesViewModel.class);
+        favoritesViewModel.init();
         View root = inflater.inflate(R.layout.fragment_favorites, container, false);
+        mFavoritesRecyclerView = root.findViewById(R.id.favorites_recycler_view);
+        mProgressBar = root.findViewById(R.id.progress_bar);
+
+
+        initRecyclerView();
+
+        favoritesViewModel.getUserPlaces().observe(getViewLifecycleOwner(), new Observer<List<VisitedPlaces>>() {
+            @Override
+            public void onChanged(List<VisitedPlaces> placeVisiteds) {
+                mAdapter.notifyDataSetChanged();
+            }
+        });
+
         final TextView textView = root.findViewById(R.id.text_favorites);
         favoritesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -30,6 +65,19 @@ public class FavoritesFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
         return root;
     }
+
+
+
+    private void showProgressBar() {
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.GONE);
+    }
+
+
 }
--- FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/ui/favorites/FavoritesViewModel.java
+++ FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/ui/favorites/FavoritesViewModel.java
@@ -1,19 +1,43 @@
 package com.bhancock.finalprojectapplication.ui.favorites;
 
+import android.widget.ProgressBar;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.bhancock.finalprojectapplication.model.VisitedPlaces;
+import com.bhancock.finalprojectapplication.repository.VisitedPlacesRepository;
+
+import java.util.List;
+
 public class FavoritesViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private MutableLiveData<ProgressBar> mProgressBar;
+    private MutableLiveData<List<VisitedPlaces>> mUserPlaces;
+    private VisitedPlacesRepository mRepository;
 
     public FavoritesViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is the favorites fragment");
     }
 
+    public void init() {
+        if (mUserPlaces != null) {
+            //I've already retrieved the data
+            return;
+        }
+        mRepository = VisitedPlacesRepository.getInstance();
+        mUserPlaces = mRepository.getUserPlaces();
+    }
+
+
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public LiveData<List<VisitedPlaces>> getUserPlaces() {
+        return mUserPlaces;
     }
 }
