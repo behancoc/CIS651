1e200f1c0d6b7dcf7eb96304def3fcfacc67e57f
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-06-07 00:31:55 -0700

    Moving everything from Main Activity into Home Activity... necessary for setting up Login Screen using the Login Activity

--- FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/MainActivity.java
+++ FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/MainActivity.java
@@ -1,8 +1,14 @@
 package com.bhancock.finalprojectapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Toast;
 
+import com.bhancock.finalprojectapplication.ui.login.LoginActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -14,20 +20,76 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_search, R.id.navigation_explore, R.id.navigation_home,
-                R.id.navigation_favorites, R.id.navigation_profile)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_search, R.id.navigation_explore, R.id.navigation_home,
+//                R.id.navigation_favorites, R.id.navigation_profile)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(navView, navController);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        bypassUserLogin();
+        //testUserLogin();
+    }
+
+    public void bypassUserLogin() {
+        new CountDownTimer(3000, 1000) {
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                finish();
+            }
+        }.start();
+    }
+
+    public void testUserLogin() {
+        new CountDownTimer(3000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+                if(mFirebaseUser == null) {
+                    Toast.makeText(getApplicationContext(),
+                            "No user found",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this,
+                            LoginActivity.class));
+                    finish();
+                }
+                else {
+
+                    if(mFirebaseUser.isEmailVerified()) {
+                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        finish();
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(),
+                                "Please verify your email and login",
+                                Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                        finish();
+                    }
+                }
+            }
+        }.start();
+    }
 }
