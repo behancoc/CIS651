5a644f0354bc2dbead505de4d560c199becc58e7
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-06-06 21:11:22 -0700

    Adapter for visited places the user has been

---
+++ FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/adapter/VisitedPlacesAdapter.java
@@ -0,0 +1,70 @@
+package com.bhancock.finalprojectapplication.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bhancock.finalprojectapplication.R;
+import com.bhancock.finalprojectapplication.model.VisitedPlaces;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class VisitedPlacesAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private Context mContext;
+    private List<VisitedPlaces> mPlaceVisited = new ArrayList<>();
+
+
+    public VisitedPlacesAdapter(Context context, List<VisitedPlaces> placeVisiteds) {
+        mContext = context;
+        mPlaceVisited = placeVisiteds;
+    }
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.favorites_list_item_layout, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        // Set the name of the 'NicePlace'
+        ((ViewHolder)holder).mName.setText(mPlaceVisited.get(position).getTitle());
+
+        // Set the image
+        RequestOptions defaultOptions = new RequestOptions()
+                .error(R.drawable.ic_launcher_background);
+        Glide.with(mContext)
+                .setDefaultRequestOptions(defaultOptions)
+                .load(mPlaceVisited.get(position).getImageUrl())
+                .into(((ViewHolder)holder).mImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPlaceVisited.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private CircleImageView mImage;
+        private TextView mName;
+
+        public ViewHolder(View view) {
+            super(view);
+            mImage = view.findViewById(R.id.image);
+            mName = view.findViewById(R.id.location_name);
+        }
+    }
+}
