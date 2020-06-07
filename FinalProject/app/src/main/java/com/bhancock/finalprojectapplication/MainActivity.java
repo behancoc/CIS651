6cad2cdd6ae09c40928887ca8d7383f6da10d131
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-06-06 20:14:25 -0700

    Rename Favorites to Feed per app specification in menu bar layout

--- FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/MainActivity.java
+++ FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_search, R.id.navigation_explore, R.id.navigation_home,
-                R.id.navigation_favorites, R.id.navigation_profile)
+                R.id.navigation_feed, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
