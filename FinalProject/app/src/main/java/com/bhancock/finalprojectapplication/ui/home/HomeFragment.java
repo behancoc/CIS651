99fd63547c6516f357d83d7f14d12ea0259ce818
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-06-06 18:07:29 -0700

    Creating recycler view in home fragment

--- FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/ui/home/HomeFragment.java
+++ FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/ui/home/HomeFragment.java
@@ -4,32 +4,58 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bhancock.finalprojectapplication.R;
+import com.bhancock.finalprojectapplication.adapter.RecyclerAdapter;
+import com.bhancock.finalprojectapplication.model.UserPlaces;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private RecyclerView mRecyclerView;
+    private RecyclerAdapter mAdapter;
+    private ProgressBar mProgressBar;
+
+    private void initRecyclerView() {
+        mAdapter = new RecyclerAdapter(getContext(), new ArrayList<UserPlaces>());
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        mRecyclerView = root.findViewById(R.id.recycler_view);
+
+        initRecyclerView();
+
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
+
+
+
     }
 }
