d1130d9bde36d369b8cd98cb1fa8dbefa7149267
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-06-08 16:39:45 -0700

    bypass login for the moment

--- FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/MainActivity.java
+++ FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/MainActivity.java
@@ -36,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        //bypassUserLogin();
-        testUserLogin();
+        bypassUserLogin();
+        //testUserLogin();
     }
 
     public void bypassUserLogin() {
