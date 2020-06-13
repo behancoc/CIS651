2d2ff1955efa2b730091b79b1c5f1ca664f8b66f
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-06-12 21:40:39 -0700

    Clean up import statements

--- FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/MainActivity.java
+++ FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/MainActivity.java
@@ -5,15 +5,10 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
