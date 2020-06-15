1422bf317f0ff9dc2c6a88c8ef12c94b98d72558
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-06-14 18:09:40 -0700

    Additional code cleanup

--- FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/ui/home/HomeViewModel.java
+++ FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/ui/home/HomeViewModel.java
@@ -6,9 +6,7 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.bhancock.finalprojectapplication.model.Video;
 import com.bhancock.finalprojectapplication.model.VisitedPlaces;
-import com.bhancock.finalprojectapplication.repository.VideoRepository;
 import com.bhancock.finalprojectapplication.repository.VisitedPlacesRepository;
 
 import java.util.List;
@@ -18,12 +16,12 @@ public class HomeViewModel extends ViewModel {
     private static final String TAG = HomeViewModel.class.getSimpleName();
 
     private MutableLiveData<String> mText;
-    private MutableLiveData<ProgressBar> mProgressBar;
+
     private MutableLiveData<List<VisitedPlaces>> mUserPlaces;
-    private MutableLiveData<List<Video>> mVideoList;
-    private MutableLiveData<List<Video>> mHomeFeedList;
+
+
     private VisitedPlacesRepository mRepository;
-    private VideoRepository mVideoRepository;
+
 
 
 
@@ -32,23 +30,10 @@ public class HomeViewModel extends ViewModel {
         mText.setValue("This is the home fragment");
     }
 
-    public void init() {
-        if (mVideoList != null) {
-            //I've already retrieved the data
-            return;
-        }
-        mVideoRepository = VideoRepository.getInstance();
-       //mHomeFeedList = mVideoRepository.getVideoObjects();
-        mVideoList = mVideoRepository.getVideoObjects();
-
-    }
 
     public LiveData<String> getText() {
         return mText;
     }
 
 
-    public LiveData<List<Video>> getVideoList() {
-        return mVideoList;
-    }
 }
