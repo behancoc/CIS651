9dd5f7feebbc380ab9c91bbc91c023140f42b36d
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-06-15 17:03:51 -0700

    Final Submission

--- FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/PermissionUtils.java
+++ FinalProject/app/src/main/java/com/bhancock/finalprojectapplication/PermissionUtils.java
@@ -22,17 +22,10 @@
 
 package com.bhancock.finalprojectapplication;
 
-import android.Manifest;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.widget.Toast;
 
+import android.content.pm.PackageManager;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.DialogFragment;
 
 public abstract class PermissionUtils {
 
